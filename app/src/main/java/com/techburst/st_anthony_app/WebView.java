e1dc1593bbb2a54eb3183dace3901f33fa4575ef
abirchanda <jbmmatrix.150@gmail.com>
2023-09-17 21:57:19 +0530

    commit

--- app/src/main/java/com/techburst/st_anthony_app/WebView.java
+++ app/src/main/java/com/techburst/st_anthony_app/WebView.java
@@ -3,8 +3,10 @@ package com.techburst.st_anthony_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebViewClient;
 
 import com.techburst.st_anthony_app.databinding.ActivityWebViewBinding;
@@ -30,7 +32,29 @@ public class WebView extends AppCompatActivity {
         binding.webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         binding.webView.clearCache(true);
         binding.webView.clearHistory();
-        binding.webView.setWebViewClient(new WebViewClient());
+        binding.webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(android.webkit.WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                binding.progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(android.webkit.WebView view, String url) {
+                super.onPageFinished(view, url);
+                binding.progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        // Set WebChromeClient for progress updates
+        binding.webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(android.webkit.WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                binding.progressBar.setProgress(newProgress);
+            }
+        });
+
         binding.webView.loadUrl(url); // Replace with the desired URL
     }
 
